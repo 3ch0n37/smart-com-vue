68e7194cfbe79d85908927196a58795d3ee724c3
Vladimir Rujević <vladimir.rujevic@live.com>
2022-07-27 16:12:52 +0200

    Add Bootstrap - built basic template for the app - remove styling created by vue-create

--- smart-com-vue/src/App.vue
+++ smart-com-vue/src/App.vue
@@ -1,47 +1,29 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <div class="container">
+      <h2>Smart Com challenge</h2>
+      <p>Lists with drag-drop functionality. Made using Vue, Typescript, Bootstrap and native D&D APIs</p>
+      <div class="row">
+        <div class="col-6">
+          <ul class="list-group">
+            <li class="list-group-item">AAA</li>
+          </ul>
+        </div>
+        <div class="col-6">
+          <ul class="list-group">
+            <li class="list-group-item">BBB</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>

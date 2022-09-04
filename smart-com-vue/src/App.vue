3a71cbf2de178f925c4bfd56806e4ca26cd89d11
Vladimir Rujević <vladimir.rujevic@live.com>
2022-09-04 22:15:39 +0200

    Add VueDraggable

--- smart-com-vue/src/App.vue
+++ smart-com-vue/src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
-import ListComponent from './components/ListComponent.vue';
 import type {ListItem} from "@/class/ListItem";
 import {computed, ref} from "vue";
 import type {Ref} from "vue";
+import SortableComponent from "./components/SortableComponent.vue";
 
 const listContents: Ref<Array<ListItem>> = ref([
   {text: 'AAA', id: 0, listId: 1},
@@ -44,21 +44,13 @@ function onDrop(evt: DragEvent, list: number) {
   <main>
     <div class="container">
       <h2>Smart Com challenge</h2>
-      <p>Lists with drag-drop functionality. Made using Vue, Typescript, Bootstrap and native D&amp;D APIs</p>
-      <div class="row">
+      <p>Lists with drag-drop functionality. Made using Vue, Typescript, Bootstrap and Sortable.JS</p>
+      <div class=row>
         <div class="col-6">
-          <ListComponent
-              :list-items="list1"
-              @startDrag="(evt, item) => {startDrag(evt, item)}"
-              @onDrop="(evt) => {onDrop(evt, 1)}"
-          />
+          <SortableComponent :list-items="list1" />
         </div>
         <div class="col-6">
-          <ListComponent
-              :list-items="list2"
-              @startDrag="(evt, item) => {startDrag(evt, item)}"
-              @onDrop="(evt) => {onDrop(evt, 2)}"
-          />
+          <SortableComponent :list-items="list2" />
         </div>
       </div>
     </div>
---
+++ smart-com-vue/src/components/SortableComponent.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+    import type { ListItem } from '@/class/ListItem';
+    import { reactive, ref } from 'vue';
+    import draggable from 'vuedraggable'
+
+    defineProps<{
+        listItems: Array<ListItem>
+    }>()
+    
+    const items = reactive([
+            {text: "AAA", id: 1},
+            {text: "BBB", id: 2}
+        ])
+    const drag = ref(false);
+</script>
+    
+<template>
+    <draggable 
+    class="list-group p-3"
+    v-model="listItems" 
+    group="items" 
+    @start="drag = true"
+     @end="drag = false" 
+     item-key="id">
+        <template #item="{ element }">
+            <div class="list-group-item">{{element.text}}</div>
+        </template>
+    </draggable>
+    <pre>{{listItems}}</pre>
+</template>
+    
+<style scoped>
+    .list-group {
+    background-color: #eee;
+  }
+</style>
